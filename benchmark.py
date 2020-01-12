f007e1245108d7ce9d0eac991a249b8f0b58f310
loreloc <lorenzoloconte@outlook.it>
2020-01-13 00:56:49 +0100

    improved benchmarks speed

--- benchmark.py
+++ benchmark.py
@@ -1,10 +1,10 @@
 # Other models benchmark for comparison
 
 import numpy as np
-import sklearn as sk
-import sklearn.metrics
-import sklearn.model_selection
-import sklearn.preprocessing
+from sklearn.model_selection import cross_validate
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import precision_recall_fscore_support
+
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.neural_network import MLPClassifier
@@ -20,8 +20,6 @@ x_train, x_test, y_train, y_test = load_koi_dataset()
 x_data = np.concatenate((x_train, x_test))
 y_data = np.concatenate((y_train, y_test))
 
-scores = [ 'precision', 'recall', 'f1' ]
-
 models = {
     'knn': KNeighborsClassifier(),
     'svc': SVC(),
@@ -29,15 +27,19 @@ models = {
     'rfc': RandomForestClassifier()
 }
 
-cv = sk.model_selection.StratifiedKFold(5)
+cv = StratifiedKFold(5)
 
 def evaluate_model(model):
-    results = {}
-    for score in scores:
-        results[score] = sk.model_selection.cross_val_score(
-            model, x_data, y_data, cv=cv, scoring=score, verbose=1
-        ).mean()
-    return results
+    scores = cross_validate(
+        model, x_data, y_data, cv=cv,
+        scoring=['precision', 'recall', 'f1'], verbose=1
+    )
+
+    return {
+        'precision': scores['test_precision'].mean(),
+        'recall': scores['test_recall'].mean(),
+        'f1': scores['test_f1'].mean()
+    }
 
 for k in models:
     values = evaluate_model(models[k])
